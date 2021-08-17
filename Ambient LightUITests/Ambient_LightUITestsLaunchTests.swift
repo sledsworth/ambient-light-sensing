09c3f2625779338b8c8d5ea458a0f8f96a163d34
Steve Ledsworth <sledsworth@gmail.com>
2021-08-16 20:56:40 -0400

    Initial Commit

---
+++ Ambient LightUITests/Ambient_LightUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Ambient_LightUITestsLaunchTests.swift
+//  Ambient LightUITests
+//
+//  Created by Steve Ledsworth on 8/16/21.
+//
+
+import XCTest
+
+class Ambient_LightUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
