703b123b36471894a17b993ee6546b27544f1b3e
Steve Ledsworth <sledsworth@gmail.com>
2021-09-19 16:57:42 -0400

    initial commit
    ARKit ambient light sensing. Not much resolution on the higher end. A well lit room and a sunny day outdoors produce the same value.

---
+++ Ambient Light/AmbientLightManager.swift
@@ -0,0 +1,13 @@
+//
+//  AmbientLightManager.swift
+//  AmbientLightManager
+//
+//  Created by Steve Ledsworth on 8/16/21.
+//
+
+import Foundation
+import ARKit
+
+class AmbientLightManager {
+    
+}
--- Ambient Light/ContentView.swift
+++ Ambient Light/ContentView.swift
@@ -7,30 +7,75 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
+    @State var text: String = "test"
+    
     var body: some View {
-        return ARViewContainer().edgesIgnoringSafeArea(.all)
+        print(text)
+        return ZStack {
+            ARViewContainer(text: $text).edgesIgnoringSafeArea(.all)
+            VStack(alignment: .leading) {
+                Text(text)
+                    .monospacedDigit()
+                    .bold()
+                    .padding()
+                    .frame(width: 200)
+                    .background(.bar)
+                    .cornerRadius(16)
+                Spacer()
+            }
+        }
+    }
+}
+
+class Coordinator: NSObject, ARSessionDelegate {
+
+    @Binding var text: String
+
+    init(text: Binding<String>) {
+        _text = text
+    }
+    
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        let ambientLight = session.currentFrame?.lightEstimate?.ambientIntensity
+        if let light = ambientLight?.exponent {
+//            print(light.description)
+            text = light.description
+        }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
     
+    @Binding var text: String
+
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(text: $text)
+    }
+    
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
+        arView.session.configuration?.isLightEstimationEnabled = true
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+//        let boxAnchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+//        arView.scene.anchors.append(boxAnchor)
+        
+        arView.session.delegate = context.coordinator
         
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+//        uiView.text = text
+    }
     
 }
 
